28227c32dae6870992fb73c4861ab84f80c76d1c
Sahil Shaikh <sahil68shaikh68@gmail.com>
2026-01-24 19:39:41 +0530

    Hirepilot img

--- app/page.tsx
+++ app/page.tsx
@@ -342,7 +342,7 @@ export default function Portfolio() {
     "Google OAuth",
     "Full-Stack SaaS"
   ]}
-  image="/hirepilot-dashboard.png"
+  image="/hirepilot-dashboard.jpeg"
   demoUrl="https://hirepilotbysahil.vercel.app"
   repoUrl="https://github.com/SahilShaikh-7/hirepilotbysahil"
 />
